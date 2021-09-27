54aa74c209511fc6795804ab5d8ff28e7f632236
Abdoulrhman <abdoulrhman_salah@hotmail.com>
2021-09-27 09:43:29 +0200

    Adding theme config

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 
 const Home = () => {
-  return <h1 className="bg-gray-100 flex">Hello</h1>;
+  return <h1 className="bg-primary flex text-tizo">Hello</h1>;
 };
 
 export default Home;
