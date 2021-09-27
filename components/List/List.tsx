9a2ae12ab37b1e6ed86cebabbbbf4d5d0d16d261
Aml Taha <Aml.Taha@arabiansystems.com>
2021-09-27 13:07:46 +0200

    responsive

--- components/List/List.tsx
+++ components/List/List.tsx
@@ -1,90 +1,109 @@
-import React from 'react'
-import type { NextPage , NextComponentType } from 'next'
+import React from "react";
+import type { NextPage, NextComponentType } from "next";
+import Pinned from "../../public/assets/icons/pinnedNew.svg";
+import Noti from "../../public/assets/icons/notification.svg";
+import Share from "../../public/assets/icons/shareNew.svg";
+import Clock from "../../public/assets/icons/clock.svg";
+import Dots from "../../public/assets/icons/dots.svg";
+import bidGray from "../../public/assets/icons/bidGray.svg";
+import heart from "../../public/assets/icons/haert.svg";
+import speed from "../../public/assets/icons/speed.svg";
 
-import styles from './styles.module.scss'
 
-const  Header : NextComponentType =()=> {
-    return (
-        <div className={styles.warpper}>
-            <div className={styles.list}>
-                <div className={styles.list_img}>
-                    <div className={ styles.image_Wrapper } >
-                    <img src="https://images.pexels.com/photos/1149137/pexels-photo-1149137.jpeg?cs=srgb&dl=pexels-kelson-downes-1149137.jpg&fm=jpg"/>
-                        <div className={styles.cartBadge}>
-                            
-                      Lot # 589280
-                        </div>
-                        <div className={styles.heartWrapper}>
-                            <div className={styles.heartStatus}>
-                        	<label className={styles.heartStatusText}>
-						Added to watchlist
-						
-                                </label>
-                                </div>
-                        </div>
-                        </div>
-                </div>
-                <div className={styles.list_details}>
-                    <div className={styles.list_title}>
-                        <h3>2021 Porsche Cayen</h3>
-                        <div >
-                            <ul className={styles.cartSocail}>
-                                <li>
-1
-                                </li>
-                                                               <li>
-1
-                                </li>
-                            </ul>
-                        </div>
-                        
-                    </div>
-              
-              <div className={styles.proudectStatus}>
-								<label
-									className={styles.labelStatus} 
-								
-								>
-									
-									<span>Luxury</span>
-                        </label>
-                        
-                    </div>
+import styles from "./styles.module.scss";
 
-                    <div className={styles.stats}>
-                        <ul className={styles.statsList}>
-                            <li>
-ss
-                            </li>
-                                                      <li>
-ss
-                            </li>                          <li>
-ss
-                            </li>                          <li>
-ss
-                            </li>
-</ul>
-                    </div>
-               
-                  </div>
-                <div className={styles.list_btn}>
-                    <span className={styles.price_amount}>
-                     <sup className={styles.uppercase_text}>AED</sup>
-                     <label className={styles.amount_label}>121,000</label>
-                    </span>
-                    <div className={styles.priceStyle}>
-                        <span>Min Increment :</span><span className={styles.secoundSpan}>
- 100
-                        </span>
-                      
-                    </div>
-                      <button className={styles.bidBtn}>
-                            Bid Now
-                        </button>
-                </div>
+const Header: NextComponentType = () => {
+  return (
+    <div className={styles.warpper}>
+      <div className={styles.list}>
+        <div className={styles.list_img}>
+          <div className={styles.image_Wrapper}>
+            <img src="https://images.pexels.com/photos/1149137/pexels-photo-1149137.jpeg?cs=srgb&dl=pexels-kelson-downes-1149137.jpg&fm=jpg" />
+            <div className={styles.cartBadge}>Lot # 589280</div>
+            <div
+              className={
+               styles.heartWrapperAr
+              }
+            >
+              <div className={styles.heartStatus}>
+                <label className={styles.heartStatusText}>
+                  <img
+                    className={styles.heartIcon}
+                    src="/assets/icons/haert.svg"
+                  />
+                  <span>Added to watchlist</span>
+                </label>
+              </div>
             </div>
+          </div>
+        </div>
+        <div className={styles.list_details}>
+          <div className={styles.list_title}>
+            <h3>2021 Porsche Cayen</h3>
+            <div>
+              <ul className={`${styles.cartSocail} ${styles.hiddenTablet}`}>
+                <li
+                  className={`${styles.cartSocailList} ${styles.backgroundPinned}`}
+                >
+                  <img src="/assets/icons/pinnedNew.svg" />
+                </li>
+                <li
+                  className={`${styles.cartSocailList} ${styles.backgroundGray}`}
+                >
+                  <img src="/assets/icons/shareNew.svg" />
+                </li>
+                <li
+                  className={`${styles.cartSocailList} ${styles.backgroundGray}`}
+                >
+                  <img src="assets/icons/notification.svg" />
+                </li>
+              </ul>
+              <ul className={`${styles.cartSocail} ${styles.showTablet}`}>
+                <li className={styles.cartSocailList}>
+                  <img src="/assets/icons/dots.svg" />
+                </li>
+              </ul>
             </div>
-    )
-}
+          </div>
 
-export default Header
+          <div className={styles.proudectStatus}>
+            <label className={styles.labelStatus}>
+              <span>Luxury</span>
+            </label>
+          </div>
+
+          <div className={styles.stats}>
+            <ul className={styles.statsList}>
+              <li>
+                <img src="assets/icons/bidGray.svg" />
+                <span>364</span>
+              </li>
+              <li>
+                <img src="assets/icons/clock.svg" />
+                <span>364</span>
+              </li>
+              <li>
+                <img src="assets/icons/speed.svg" />
+                <span>364</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.list_btn}>
+          <span className={styles.price_amount}>
+            <sup className={styles.uppercase_text}>AED</sup>
+            <label className={styles.amount_label}>121,000</label>
+          </span>
+          <div className={styles.priceStyle}>
+            <span>Min Increment :</span>
+            <sub> AED</sub>
+            <span className={styles.secoundSpan}>100</span>
+          </div>
+          <button className={styles.bidBtn}>Bid Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
