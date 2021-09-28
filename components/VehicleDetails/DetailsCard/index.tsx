db71eab7b562d6568222cff973d9a517a7ebced8
eman.fouad <eman.fouad@emiratesauction.ae>
2021-09-28 13:59:03 +0200

    card-details-branch

---
+++ components/VehicleDetails/DetailsCard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './styles.module.scss'
+
+function DetailsCard() {
+    return (
+        <div className={styles.test}>
+            <p>for testing</p>
+        </div>
+    )
+}
+
+export default DetailsCard
