bf14b40bcc9f2b07d3e46ac0e02f2884255377e8
Aml Taha <Aml.Taha@arabiansystems.com>
2021-09-28 15:02:32 +0200

    checkbox style

---
+++ components/AuctionaAlerts/AuctionaAlerts.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import type { NextPage, NextComponentType } from "next";
+import styles from "./styles.module.scss";
+import Close from "../../public/assets/icons/close.svg";
+
+const AuctionAlerts: NextComponentType = () => {
+  return (
+    <div>
+      <div className={styles.auctionAlerts}>
+        <div className={styles.modal_content}>
+          <div className={styles.modal_header}>
+            <img src="assets/icons/close.svg" />
+          </div>
+          <div className={styles.modal_body}>
+            <h3>Enable Auction Alerts</h3>
+            <p>How would you like to be notified of new auctions</p>
+            <div className={styles.check}>
+              <label className={styles.container}>
+                <input type="checkbox" />
+                <span className="checkmark"></span>
+              </label>
+              <div className={styles.info}>
+                <p>Email Address</p>
+                <label>johnson.85@gmail.com</label>
+              </div>
+            </div>
+            {/* Check Div */}
+            <div className={styles.check}>
+              <label className={styles.container}>
+                <input type="checkbox" />
+                <span className="checkmark"></span>
+              </label>
+              <div className={styles.info}>
+                <p>Email Address</p>
+                <label>johnson.85@gmail.com</label>
+              </div>
+            </div>
+            {/* Check Div  */}
+            <div className={styles.check}>
+              <label className={styles.container}>
+                <input type="checkbox" />
+                <span className={styles.checkmark}></span>
+              </label>
+              <div className={styles.info}>
+                <p>Email Address</p>
+                <label>johnson.85@gmail.com</label>
+              </div>
+            </div>
+            {/* Check Div  */}
+          </div>
+
+          <div className="styles.modal-footer">
+            <button>Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuctionAlerts;
---
+++ pages/AuctionAlerts/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AuctionAlerts from "../../components/AuctionaAlerts/AuctionaAlerts";
+
+function ResultsPage() {
+  return (
+    <div>
+      <AuctionAlerts/>
+    </div>
+  );
+}
+
+export default ResultsPage;
