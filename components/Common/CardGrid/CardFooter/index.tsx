012bae9825de0a7970a04abd08bfc1f08b0b675d
eman.fouad <eman.fouad@emiratesauction.ae>
2021-09-28 10:59:10 +0200

    edit style

--- components/Common/CardGrid/CardFooter/index.tsx
+++ components/Common/CardGrid/CardFooter/index.tsx
@@ -7,11 +7,11 @@ function CardFooter({dir}:any) {
         <div style={{direction:dir}} className={styles.cardFooter}>
             <div className={styles.itemContainer}>
                 <img className={styles.footerImage} src="/assets/icons/bid-grey.svg"  />
-                <p style={{marginLeft: dir == "ltr" ? "5px" : "0px", marginRight: dir == "rtl" ? "5px" : "0px"}} className={styles.footerText}>367</p>
+                <p className={styles.footerText}>367</p>
             </div>  
             <div className={styles.itemContainer}>
                 <img className={styles.footerImage} src="/assets/icons/clock.svg"  />
-                <p style={{marginLeft: dir == "ltr" ? "5px" : "0px", marginRight: dir == "rtl" ? "5px" : "0px"}} className={styles.footerText}>14d:11h:11m</p>
+                <p className={styles.footerText}>14d:11h:11m</p>
             </div>    
     </div>
     )
