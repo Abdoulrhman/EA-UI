c8b4acf6b687130d3b81136388092038d62fbd01
eman.fouad <eman.fouad@emiratesauction.ae>
2021-09-29 16:14:57 +0200

    added highlight card

---
+++ components/VehicleDetails/Highlights/index.tsx
@@ -0,0 +1,64 @@
+import React,{useState} from 'react'
+import Divider from '../../Common/Atoms/Divider'
+import styles from './styles.module.scss'
+
+function Highlights() {
+    const [info,setInfo] = useState({
+        ViewTypeId: 1,
+        Collapsed: false,
+        EnableCollapse: true,
+        OptionGroups: [
+            {
+                Id: 12,
+                Title: "Condition",
+                ImageUrl: "",
+                Order: 0,
+                Options: [
+                    {
+                        Id: 0,
+                        Title: "GCC Specifications"
+                    }
+                ]
+            },
+            {
+                Id: 13,
+                Title: "General",
+                ImageUrl: "",
+                Order: 0,
+                Options: []
+            },
+            {
+                Id: 14,
+                Title: "Region",
+                ImageUrl: "",
+                Order: 0,
+                Options: []
+            }
+        ],
+        Id: 1,
+        Title: "HIGHLIGHTS"
+    })
+
+    return (
+        <div className={styles.container}>
+            {info.OptionGroups.map(
+                (o) =>
+                    o.Options.length > 0 && (
+                        <>
+                            <h4 className={styles.optionTitle}>{o.Title}</h4>
+                            <ul className={styles.optionsContainer}>
+                                {o.Options.map((option) => (
+                                    <li className={styles.optionItem}>
+                                        {option.Title}
+                                    </li>
+                                ))}
+                            </ul>
+                            <Divider width="100%" height="1px" bgColor="#EDEDED" margin="20px" />
+                        </>
+                    )
+            )}      
+        </div>
+    )
+}
+
+export default Highlights
--- pages/Eman-page/index.tsx
+++ pages/Eman-page/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import GridCard from '../../components/Common/CardGrid'
 import CardInformation from '../../components/VehicleDetails/CardInformation'
+import Highlights from '../../components/VehicleDetails/Highlights'
 
 function index() {
     return (
         <div>
             {/* <GridCard /> */}
-            <CardInformation />
+            {/* <CardInformation /> */}
+            <Highlights />
         </div>
     )
 }
